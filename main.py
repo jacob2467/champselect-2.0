9dc1eeff6e588287821eb0020d1697643f9261fd
jacob2467 <92223080+jacob2467@users.noreply.github.com>
2024-11-17 18:37:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import api
+import configparser
+from lcu_driver import Connector
+
+config = conf
+
+client = Connector()
+
+@client.ready
+async def lcu_ready(connection):
+    summoner = await connection.request("get", "/lol-summoner/v1/current-summoner")
+    print(await summoner.json())
 
 
-loaded_in = False
-while not loaded_in:
-    state = api.get_gamestate()
+@client.ws.register("/lol-matchmaking/v1/ready-check", event_types=("UPDATE",))
+async def accept_match(connection, event):
+    if event.data["playerResponse"] == "None":
+        await connection.request("post", "/lol-matchmaking/v1/ready-check/accept")
+        print("Match has been accepted!")
 
-    match state:
-        case "queue":
-            match_found = True  # dummy value
-            if match_found:
-                api.accept_match()
-        case "champselect":
-            pass
+
+client.start()
