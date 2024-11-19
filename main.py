562eb0821e5289312fb2cd271b7286eb28d3c602
jacob2467 <92223080+jacob2467@users.noreply.github.com>
2024-11-18 19:08:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,18 @@ in_game = False
 last_action = None
 last_gamestate = None
 
-# temp
-summoner_id = 42069
-endpoints = {
-    "gamestate": "/lol-gameflow/v1/gameflow-phase",  # GET
-    "start_queue": "/lol-lobby/v2/lobby/matchmaking/search",  # POST
-    "match_found": "/lol-matchmaking/v1/ready-check",  # GET
-    "accept_match": "/lol-matchmaking/v1/ready-check/accept",  # POST
-    "champselect_session": "/lol-champ-select/v1/session",  # GET
-    "champselect_action": "/lol-champ-select/v1/session/actions/",  # PATCH
-    "owned_champs": "/lol-champions/v1/owned-champions-minimal",  # GET
-    "current_summoner": "/lol-summoner/v1/current-summoner",  # GET
-    "all_champs": f"/lol-champions/v1/inventories/{summoner_id}/champions",  # GET
-    "pickable_champs": "/lol-champ-select/v1/pickable-champions"  # GET
-}
-
+output_filepath = "C:/Users/jamo0/Desktop/text.txt"
 
 def save_to_file(header, text):
-    with open("C:/Users/jamo0/Desktop/text.txt", "w") as file:
+    with open(output_filepath, "a") as file:
         string = f"{header}: \n{text}\n"
         file.write(string)
         print(string)
 
+
+# Initialize output file
+with open(output_filepath, "w") as file:
+    file.write("Champ select data: \n")
 
 while not in_game:
     time.sleep(1)
@@ -40,7 +30,7 @@ while not in_game:
 
     match gamestate:
         case "Lobby":
-            c.reset_after_dodge()
+            pass
 
         case "ReadyCheck":
             c.reset_after_dodge()
@@ -49,7 +39,6 @@ while not in_game:
         case "ChampSelect":
             session = c.get_session()
 
-            my_cellid = c.get_localcellid()
             action = c.find_action()
             action_type = action["type"]
 
@@ -59,6 +48,7 @@ while not in_game:
             last_action = action_type
             save_to_file("Action type: ", action_type)
 
+            # Handle each champ select phase
             match action_type:
                 case "pick":
                     if c.can_pick(action):
