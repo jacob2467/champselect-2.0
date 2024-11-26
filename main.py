c732ea095ccc6dc2d89a6887ad8825306c7aac3d
jacob2467 <92223080+jacob2467@users.noreply.github.com>
2024-11-26 02:59:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import connect
+import requests
 import time
 c = connect.Connection()
 in_game = False
@@ -9,48 +10,53 @@ last_session = None
 
 while not in_game:
     time.sleep(1)
-    gamestate = c.api_get("gamestate").json()
+    # Wrap the loop in a try block to catch errors when the client closes
+    try:
+        gamestate = c.api_get("gamestate").json()
 
-    # Print current gamestate if it's different from the last one
-    if gamestate != last_gamestate:
-        print("Current gamestate:", gamestate)
-        last_gamestate = gamestate
-
-
-    match gamestate:
-        case "Lobby":
-            c.reset_after_dodge()
-            # TODO: Start queue
-
-        case "ReadyCheck":
-            c.accept_match()
-            # pass
-
-        case "ChampSelect":
-            # Stop error that can happen if this block of code is run immediately after someone dodges
-            try:
-                phase = c.get_champselect_phase()
-                c.update_actions()
-                print(phase)
-            except:
-                phase = "skip"
+        # Print current gamestate if it's different from the last one
+        if gamestate != last_gamestate:
+            print("Current gamestate:", gamestate)
+            last_gamestate = gamestate
 
 
-            # Handle each champ select phase
-            match phase:
-                case "PLANNING":
-                    c.hover_champ()
-                case "BAN_PICK":
-                    c.ban_or_pick()
-                case "FINALIZATION":
-                    c.send_runes()
-                    c.send_summs()
-                case "skip":
-                    pass
+        match gamestate:
+            case "Lobby":
+                c.reset_after_dodge()
+                # TODO: Start queue
 
-        # End loop if a game starts
-        case "InProgress":
-            in_game = True
+            case "ReadyCheck":
+                c.accept_match()
+                # pass
 
-        case default:
-            pass
+            case "ChampSelect":
+                # Stop error that can happen if this block of code is run immediately after someone dodges
+                try:
+                    phase = c.get_champselect_phase()
+                    c.update_actions()
+                    c.update_champ_intent()
+                    print(phase)
+                except:
+                    phase = "skip"
+
+
+                # Handle each champ select phase
+                match phase:
+                    case "PLANNING":
+                        c.hover_champ()
+                    case "BAN_PICK":
+                        c.ban_or_pick()
+                    case "FINALIZATION":
+                        c.send_runes()
+                        c.send_summs()
+                    case "skip":
+                        pass
+
+            # End loop if a game starts
+            case "InProgress":
+                in_game = True
+
+            case default:
+                pass
+    except requests.exceptions.ConnectionError:
+        print("Connection error.")  # TODO: Add to this
