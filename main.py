da40c0709f9e6e26e6a814f5f6f0ce66be08d7c2
jacob2467 <jamo0415@gmail.com>
2024-12-07 02:18:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import connect
 import requests
 import time
-c = connect.Connection()
-in_game = False
-last_action = None
-last_gamestate = None
-last_phase = None
-last_session = None
-champselect_loop = 0
+
+connection_initiated: bool = False
+in_game: bool = False
+last_gamestate: dict = {}
+champselect_loop: int = 0  # Keep track of how many loops run during champselect
+RETRY_RATE: int = 10  # How many seconds to wait after a failed connection attempt
+UPDATE_INTERVAL: float = .1  # How many seconds to wait before re-running the main loop
+
+# Wait for client to open if it's not open already
+while not connection_initiated:
+    try:
+        c = connect.Connection()
+        connection_initiated = True
+    # If the connection isn't successful or the lockfile doesn't exist, the client isn't open yet
+    except (requests.exceptions.ConnectionError, FileNotFoundError) as e:
+        print(f"Failed to connect to the league client - is it open? Retrying in {RETRY_RATE} seconds...")
+        time.sleep(RETRY_RATE)
+
+    except KeyError as e:
+        # Client is still loading, try again until it finishes loading
+        pass
 
 while not in_game:
-    time.sleep(1)
+    time.sleep(UPDATE_INTERVAL)
     # Wrap the loop in a try block to catch errors when the client closes
     try:
         gamestate = c.api_get("gamestate").json()
@@ -25,18 +39,22 @@ while not in_game:
             case "Lobby":
                 c.start_queue()
                 c.reset_after_dodge()
-                champselect_loop = 0
+                champselect_loop = 1
 
             case "ReadyCheck":
                 c.accept_match()
 
+
             case "ChampSelect":
-                c.update()
+                # Wrap in try block to catch KeyError when someone dodges - champselect actions don't exist anymore
+                try:
+                    c.update()
+                    phase = c.get_champselect_phase()
+                except KeyError:
+                    phase = "skip"
 
                 champselect_loop += 1
                 print(f"\nChampselect loop # {champselect_loop}:")
-
-                phase = c.get_champselect_phase()
                 print("Champselect phase:", phase)
 
                 # Handle each champ select phase separately
