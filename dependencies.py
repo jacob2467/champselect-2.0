1842bb595d851df2bd79af061069bffd0ebd0728
jacob2467 <jamo0415@gmail.com>
2024-12-28 13:20:21 -0500

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -7,7 +7,7 @@ def install_and_import(package):
         # If the package is already installed, just return it
         return importlib.import_module(package)
     except ModuleNotFoundError:
-        print(f"Please wait, installing package '{package}'...")
+        print(f"Please wait, installing package \"{package}\"...")
         # Install the package with pip
         subprocess.check_call(
             [sys.executable, "-m", "pip", "install", package],
@@ -15,6 +15,8 @@ def install_and_import(package):
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL)
         # Return the installed package
-        return importlib.import_module(package)
+        module = importlib.import_module(package)
+        print(f"Successfully installed package \"{package}\"!")
+        return module
     except Exception as e:
         print(f"Error while installing {package}: {e}")
