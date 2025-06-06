db9614bd072ad2ee90de2545385fac3750535fdc
jacob2467 <jamo0415@gmail.com>
2025-06-06 17:49:29 -0400

    Add script to install dependencies

---
+++ dependencies.py
@@ -0,0 +1,18 @@
+import subprocess
+import warnings
+
+package_names: list[str] = ["requests"]
+
+for name in package_names:
+    try:
+        print(f"\nInstalling package '{name}'...\n")
+        subprocess.run(["pip", "install", name])
+    except Exception as e:
+        warnings.warn(f"Unable to install {name} - trying pip3 instead of pip (works on some OSX installations)")
+        try:
+            subprocess.run(["pip", "install", name])
+        except Exception as e:
+            raise RuntimeError(f"Unable to install {name}: {e}")
+    print(f"\nSuccessfully installed package '{name}'!\n")
+
+input("Press any key to continue...")
